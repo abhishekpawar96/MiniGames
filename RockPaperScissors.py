e105217aabef0dec87df9f8965c3ad0d657faf99
I341341 <abhishekpawar96@gmail.com>
2020-01-11 12:50:24 +0530

    Add description for Rock Paper Scissor Lizard Spock

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,5 +1,9 @@
 import random
 
+# TODO:
+# "Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors,
+# scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock,
+# and as it always has, rock crushes scissors."
 
 while True:
     print("Make your choice")
