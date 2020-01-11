98c64bde5d1ce23033bd53c19b6ba6f0f514e902
I341341 <abhishekpawar96@gmail.com>
2020-01-11 12:48:07 +0530

    Add Rock Paper Scissors

---
+++ RockPaperScissors.py
@@ -0,0 +1,35 @@
+import random
+
+
+while True:
+    print("Make your choice")
+    choice = str(input())
+    choice = choice.lower()
+    print("Your Choice is: ", choice)
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+    print("Computer's Choice is: ", computer_choice)
+    if choice in choices:
+        if choice == computer_choice:
+            print("It's a Tie!")
+        if choice == 'rock':
+            if computer_choice == 'paper':
+                print('you lose, sorry :(')
+            elif computer_choice == 'scissors':
+                print('You win!!!!! congrats :)')
+        if choice == 'paper':
+            if computer_choice == 'scissors':
+                print('you lose, sorry :(')
+            elif computer_choice == 'rock':
+                print('You win!!!!! congrats :)')
+        if choice == 'scissors':
+            if computer_choice == 'rock':
+                print('you lose, sorry :(')
+            elif computer_choice == 'paper':
+                print('You win!!!!! congrats :)')
+        else:
+            print('invalid choice, try again')
+
+        print()
+
+
